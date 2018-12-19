1ac6d95d852b9480c1b58d01f49abb2c2953d626
Sai Pone Tha Aung <saipone@pixeldirects.com>
2018-12-19 17:27:30 +0630

    Migration fix test

--- database/migrations/2018_12_14_065045_modify_chat_button_table_change_url_from_interger_to_text.php
+++ database/migrations/2018_12_14_065045_modify_chat_button_table_change_url_from_interger_to_text.php
@@ -14,7 +14,7 @@ class ModifyChatButtonTableChangeUrlFromIntergerToText extends Migration
     public function up()
     {
         Schema::table('chat_button', function(Blueprint $table) {
-            $table->text('url')->change()->renameColumn('url_size', 'url');
+            $table->text('url_size')->change()->renameColumn('url_size', 'url');
             // $table->;
         });
     }

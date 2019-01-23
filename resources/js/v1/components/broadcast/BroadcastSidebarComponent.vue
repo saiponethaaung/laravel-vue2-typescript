e174060f171c7e3b69d308ea2f9d27ad17875738
nannsmk <kalayar@pixeldirects.com>
2019-01-23 15:13:12 +0630

    23jan2019addfile

---
+++ resources/js/v1/components/broadcast/BroadcastSidebarComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <div class="chatSbHeaderOption">
+            <div class="chatFilterList float-left">
+                <div class="inboxOptionTitle">Broadcast</div>
+            </div>
+        </div>
+
+        <div class="contentRoot"><i class="material-icons iconAlign">send</i>Send your message now</div>
+        <div class="btnClick">
+            <router-link :to="{'name': 'project.broadcast.sendnow'}">
+                Click here to send your message now
+            </router-link>
+        </div>
+
+        <div class="contentRoot">Trigger your message</div>
+        <div class="btnClick"><i class="material-icons">add</i>Add a trigger</div>
+
+        <div class="contentRoot"></i>Schedule your message</div>
+        <div class="chatBlockContentList">
+            <div class="btnClick">Happy New Year</div>
+            <div class="btnClick">Time</div>
+        </div>
+        <div class="btnClick"><i class="material-icons">add</i>Add a trigger</div>
+
+        
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class BroadcastSidebarComponent extends Vue {
+
+    mounted() {
+
+    }
+}
+</script>

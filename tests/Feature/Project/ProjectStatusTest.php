772f45513b03dd7c6e84539b324057d3e6ee8995
Sai Pone Tha Aung <saipone@pixeldirects.com>
2019-02-07 12:30:07 +0630

    Test failed sample

---
+++ tests/Feature/Project/ProjectStatusTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\Project;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\Project;
+use App\Models\ProjectUser;
+
+class ProjectStatusTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testPublishProject()
+    {
+        factory(Project::class)->create(['user_id' => $this->user->id])->each(function($project) {
+            factory(ProjectUser::class)->create([
+                'project_id' => $project->id,
+                'user_id' => $project->user_id
+            ]);
+        });
+        $this->assertTrue(false);
+    }
+}

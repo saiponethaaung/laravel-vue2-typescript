408dd2f5231b9bf2511be6d8bdafa7da7a2d74eb
Sai Pone Tha Aung <saipone@pixeldirects.com>
2019-02-08 14:55:04 +0630

    Backup for merge

---
+++ tests/Feature/Chatbot/ChatBlockTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Feature\Chatbot;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+use App\Models\ChatBlock;
+
+class ChatBlockTest extends TestCase
+{
+    public function testCreateChatBlock()
+    {
+        
+    }
+}

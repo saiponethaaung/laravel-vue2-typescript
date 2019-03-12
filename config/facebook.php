6307ca2c8e0b3cc11ed47280bb12aa7c0a2f877e
Sai Pone Tha Aung <saipone@pixeldirects.com>
2019-03-12 16:33:59 +0630

    Update test your bot token

--- config/facebook.php
+++ config/facebook.php
@@ -5,5 +5,5 @@ return [
     "appSecret" => "4b91cb69e20f589c5b344be168a9e001",
     "graphVersion" => "v3.2",
     "defaultPageId" => "2250742581846888",
-    "defaultPageToken" => "EAAQaj0N2ahcBAKtFYmhfadkadZCmL0lqMp9uFivGGD14nnAem15lGkBeLG6jGMO382p19ghrTRodyZCrOo4QZBdPLjrZC8tQD1bgaplABo47ZChIh02rMj7nW292A95dJFmZCz93hkWMgiZB2KjCK5Iwby1ZBZA8u08gN07vIIXZAAZA6OoNNItXJQn"
+    "defaultPageToken" => "EAAQaj0N2ahcBAGalhW6hpGex97MXm1xx4oZAcFRd6cuawkZBZAZCv9E9BALIfsM9pMeZChpX5vF3M6dRGtZC05oaYXtS3Xd2DyMkfwphCcu0jNQcJTE66TtbYZCyDBZCSOBqtifkuJtqLv1h9ffyHV7Y7YAAySSg5Jju5a3u3VfSjgZDZD"
 ];

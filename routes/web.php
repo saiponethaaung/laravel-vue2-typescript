d69833d69837fccd101cb0c258756da03b44af7d
Sai Pone Tha Aung <saiponethaaung@gmail.com>
2019-06-11 21:21:25 +0630

    Fix route

--- routes/web.php
+++ routes/web.php
@@ -11,55 +11,55 @@
 |
 */
 
-Route::get('/', function () {
+Route::any('/', function () {
     // return view('welcome');
     return view('spa');
 });
-Route::get('/testplugin', function () {
-    // return view('welcome');
-    return '
-    -a
-<div class="fb-send-to-messenger" 
-    messenger_app_id="1155102521322007" 
-    page_id="643637199419798" 
-    data-ref="aab3238922bcc25a6f606eb525ffdc56-643637199419798-343922309521024" 
-    color="blue" 
-    size="xlarge">
-    Test this bot
-</div>
-a
-<script>
-window.fbAsyncInit = () => {
-    FB.init({
-        appId      : \'1155102521322007\',
-        cookie     : true,
-        xfbml      : true,
-        version    : \'v3.2\'
-    });
+// Route::get('/testplugin', function () {
+//     // return view('welcome');
+//     return '
+//     -a
+// <div class="fb-send-to-messenger" 
+//     messenger_app_id="1155102521322007" 
+//     page_id="643637199419798" 
+//     data-ref="aab3238922bcc25a6f606eb525ffdc56-643637199419798-343922309521024" 
+//     color="blue" 
+//     size="xlarge">
+//     Test this bot
+// </div>
+// a
+// <script>
+// window.fbAsyncInit = () => {
+//     FB.init({
+//         appId      : \'1155102521322007\',
+//         cookie     : true,
+//         xfbml      : true,
+//         version    : \'v3.2\'
+//     });
 
-    console.log("Wet");
+//     console.log("Wet");
     
-    // FB.AppEvents.logPageView(); 
-    FB.Event.subscribe(\'send_to_messenger\', function(e) {
-        console.log("event send to messenger", e);
-    });
+//     // FB.AppEvents.logPageView(); 
+//     FB.Event.subscribe(\'send_to_messenger\', function(e) {
+//         console.log("event send to messenger", e);
+//     });
     
-};
+// };
 
-(function(d, s, id){
-    var js, fjs = d.getElementsByTagName(s)[0];
-    if (d.getElementById(id)) {
-        return;
-    }
-    js = d.createElement(\'script\');
-    js.id = id;
-    js.src = "https://connect.facebook.net/en_US/sdk.js";
-    fjs.parentNode.insertBefore(js, fjs);
-}(document, \'script\', \'facebook-jssdk\'));
-</script>';
-});
+// (function(d, s, id){
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) {
+//         return;
+//     }
+//     js = d.createElement(\'script\');
+//     js.id = id;
+//     js.src = "https://connect.facebook.net/en_US/sdk.js";
+//     fjs.parentNode.insertBefore(js, fjs);
+// }(document, \'script\', \'facebook-jssdk\'));
+// </script>';
+// });
 
-Route::get('/mid', 'TestController@getPSID');
+// Route::get('/mid', 'TestController@getPSID');
 
 Route::any('{any}/{all?}', function() {
     return view('spa');
